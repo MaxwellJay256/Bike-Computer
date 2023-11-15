59aac2660f7ff5ac433f9b2a04ac31fc7310fdad
Maxwell Jay <MaxwellJay@jaylab256.onmicrosoft.com>
2023-11-15 23:53:36 +0800

    update project structure

---
+++ main/app_main.c
@@ -0,0 +1,7 @@
+#include "app_main.h"
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+    
+}
---
+++ main/include/app_main.h
@@ -0,0 +1,8 @@
+#ifndef _APP_MAIN_H_
+#define _APP_MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif // _APP_MAIN_H_
