03f190bc4130f723d98762ce8d79818af5fe2b44
Maxwell Jay <MaxwellJay@jaylab256.onmicrosoft.com>
2023-12-07 21:00:45 +0800

    fix: revert changes

--- main/app_main.c
+++ main/app_main.c
@@ -3,30 +3,9 @@
  * @brief Main application source code
 */
 #include "app_main.h"
-#include "mpu6050.h"
 
 /// @brief Main application entry point
 void app_main(void)
 {
-    mpu6050_output_t mpu6050_out;
-
-    mpu6050_init();
-
-    while (1)
-    {
-        mpu6050_out = mpu6050_get_value();
-        /*/
-        printf("accl_xout: %d\t", mpu6050_out.accel.x);
-        printf("accl_yout: %d\t", mpu6050_out.accel.y);
-        printf("accl_zout: %d\n", mpu6050_out.accel.z);
-        //*/
-        // printf("temp_out: %d\n;", mpu6050_out.temp);
-        //*/
-        printf("gyro_xout: %d\t", mpu6050_out.gyro.x);
-        printf("gyro_yout: %d\t", mpu6050_out.gyro.y);
-        printf("gyro_zout: %d\n", mpu6050_out.gyro.z);
-        //*/
-        vTaskDelay(100 / portTICK_RATE_MS);
-    }
     
 }
